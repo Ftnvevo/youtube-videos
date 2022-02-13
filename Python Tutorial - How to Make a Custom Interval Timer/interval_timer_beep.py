6283128f05eeff3d3a2382eb88c8cac3fbf6b571
Fabio <31598206+fabiomus@users.noreply.github.com>
2022-02-13 22:24:52 +0100

    Python Tutorial - How to Make an Interval Timer

---
+++ Python Tutorial - How to Make a Custom Interval Timer/interval_timer_beep.py
@@ -0,0 +1,44 @@
+import time
+import winsound
+
+print()
+print(' *** INTERVAL TIMER ***')
+print()
+
+cycles = int(input('Number of cycles --> '))
+seconds_exercise = int(input('Seconds of exercise --> '))
+seconds_rest = int(input('Seconds of rest --> '))
+print()
+
+spaces = ' ' * 30
+print('\rGet ready, the timer will start in: 5  ', end='')
+for n in reversed(range(0, 5)):
+    time.sleep(1)
+    if n > 0:
+        print('\rGet ready, the timer will start in: {}  '.format(n), end='')
+    else:
+        print('\rGOOOOO!!' + spaces, end='')
+
+winsound.Beep(3500, 1000)
+
+for cycle in range(cycles):
+    print('\rExercise: 0' + spaces, end='')
+    for n in range(seconds_exercise):
+        time.sleep(1)
+        print('\rExercise: {}   '.format(n + 1), end='')
+    
+    winsound.Beep(3500, 500)
+
+    if cycle + 1 == cycles:
+        print('\rWELL DONE!!' + spaces, end='')
+        break
+    else:
+        print('\rRest: 0' + spaces, end='')
+        for n in range(seconds_rest):
+            time.sleep(1)
+            print('\rRest: {}  '.format(n + 1), end='')
+    
+    winsound.Beep(3500, 150)
+    winsound.Beep(3500, 150)
+
+winsound.Beep(3500, 1500)
